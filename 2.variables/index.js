30c4b9c9aa59ee50bb798bb2030e3f8b27bc538c
ImKaneki <josemgarriazo@gmail.com>
2025-07-24 08:14:17 -0500

    repasodejavascrip

---
+++ 2.variables/index.js
@@ -0,0 +1,18 @@
+//1. declarar la variavle usando la palabra reserbvada y asignadnole un nombre
+let nombreAlumno
+//2. inicializacion de la variable en este paso es cuando se le saiganra un tipo de dto a la variable
+//llamo a a variable , hago uso del operador de asignacion , ledoy el dato
+nombreAlumno="Percy"
+nombreAlumno="erick"
+
+console.log(nombreAlumno)  50 
+
+//existe uyna tecnica donde declaramos e inicializamos una variable en una sola linea de codigo 
+
+let edadAlumno=50
+const PI=3.141516
+edadAlumno="hola"
+console.log(edadAlumno)  'hola'
+console.log(PI)
+//solo se usara let cuando creamos la variable luego si deseamos modificar el valor o llamar a la variable
+solo usaremos el nombre
---
+++ 3. operadores/index.js
@@ -0,0 +1,20 @@
+//operadores unarios
+//operador de negacion
+//operador esta a la izquierda del valor y como resultado obtenemos otro valor
+let negacion=!false
+//expresion es un valor complejo que sera solucionado y nos producira un nuevo valor
+console.log()
+
+
+//operadores binarios
+//son los que evaluan dos valores y nos dan como resultado un nuevo valor
+let suma=34+20
+let resta=4-2
+let producto=4*5
+let divicion=3/3
+let potencia=3**2
+let residuo=10%3
+// tarea operadores de comparacion
+// operadores logicos
+// operador tenario
+console.log(potencia)
